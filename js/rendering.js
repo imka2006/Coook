7d3c5d3aba58f792ebc1d0f0bc03c4ed0c007e44
sexyboyfrend <31unknown12@gmail.com>
2022-07-24 15:24:32 +0600

    added json and craeted gitignore

--- js/rendering.js
+++ js/rendering.js
@@ -33,39 +33,198 @@ likeFirst.forEach((item) => {
         }
     });
 });
+const catalogWrapper = document.querySelector(".catalog__list--wrapper");
+const getData = async () => {
+    const result = await fetch("http://localhost:3000/menu__food");
+    const data = await result.json();
+    for (let i = 0; i < data.length; i++) {
+      catalogWrapper.innerHTML += `
+      <div class="catalog__block">
+      <div class="catalog__block--top center">
+          <img
+              class="catalog__icon--like catalog__icon--like-first"
+              src="./ascents/img/global/like.svg"
+              alt="like"
+          />
+          <img
+              class="catalog__icon--close"
+              src="./ascents/img/global/close.svg"
+              alt="close"
+          />
+          <img
+              class="catalog__icon--like catalog__icon--like-second"
+              src="https://www.svgrepo.com/show/292078/heart.svg"
+              alt="like"
+          />
 
-// likeFirst.forEach((item) => {
-//     item.addEventListener("click", () => {
-//         item.classList.toggle("catalog__icon--liked");
-//         if (item.classList.contains("catalog__icon--liked")) {
-//             likeSecond.style.display = "block";
-//         } else {
-//             likeSecond.style.display = "none";
-//         }
-//     });
-// })
+          <img
+              class="catalog__food--image"
+              src="${data[i].img}"
+              alt="food photo"
+          />
+      </div>
+      <div class="catalog__block--bottom">
+          <div
+              class="catalog__block--spicy center"
+          >
+              <img
+                  src="./ascents/img/global/spisy-chili.svg"
+                  alt="spisy-chili"
+                  class="catalog__spisy--icon"
+              />
+          </div>
+          <div class="catalog__active--none">
+              <h3 class="catalog__food--subtitle">
+                 ${data[i].name}
+              </h3>
+              <div class="catalog__food--data">
+                  <span
+                      class="catalog__food--gram"
+                      >${data[i].gram} г</span
+                  >
+                  <div class="dot"></div>
+                  <span
+                      class="catalog__food--calory"
+                      >${data[i].calories} ккал</span
+                  >
+                  <div class="dot"></div>
+                  <span
+                      class="catalog__food--price"
+                      ><b>${data[i].price} ₽</b></span
+                  >
+              </div>
+              <p class="catalog__food--descr">
+                  ${data[i].coment}
+              </p>
+          </div>
+          <div class="catalog__additionally">
+              <div class="swiper">
+                  <div class="swiper-wrapper">
+                      <div class="swiper-slide">
+                          <h3
+                              class="catalog__food--subtitle"
+                          >
+                          ${data[i].name}
+                          </h3>
+                          <div
+                              class="catalog__food--data"
+                          >
+                              <span
+                                  class="catalog__food--gram"
+                                  >${data[i].gram} г</span
+                              >
+                              <div
+                                  class="dot"
+                              ></div>
+                              <span
+                                  class="catalog__food--calory"
+                                  >${data[i].calories} 
+                                  ккал</span
+                              >
+                              <div
+                                  class="dot"
+                              ></div>
+                              <span
+                                  class="catalog__food--price"
+                                  ><b
+                                      >${data[i].price} ₽</b
+                                  ></span
+                              >
+                          </div>
+                          <p
+                              class="catalog__food--descr"
+                          >
+                          ${data[i].coment}
+                          </p>
 
-// likeSecond.addEventListener("click", () => {
-//     likeFirst.classList.remove("catalog__icon--liked");
-//     if (likeFirst.classList.contains("catalog__icon--liked")) {
-//         likeSecond.style.display = "block";
-//     } else {
-//         likeSecond.style.display = "none";
-//     }
-// });
-catalogBlock.forEach((item) => {
-    const catalogAdd = item.querySelectorAll(".catalog__food--add");
-    const catalogClose = item.querySelectorAll(".catalog__icon--close");
-    catalogClose.forEach((itemClose) => {
-        catalogAdd.forEach((itemTwo) => {
-            itemTwo.addEventListener("click", () => {
-                item.classList.add("active");
-                itemClose.style.display = "block";
-            });
-        });
-        itemClose.addEventListener("click", () => {
-            item.classList.remove("active");
-            itemClose.style.display = "none";
-        });
-    });
+                          <h3
+                              class="catalog__additionally--title"
+                          >
+                              Дополнительно:
+                          </h3>
+                          <div
+                              class="catalog__additionally--block row"
+                          >
+                              <span
+                                  class="catalog__additionally--info"
+                                  > ${data[i].descr}/span
+                              >
+                              <div
+                                  class="catalog__additionally--content"
+                              >
+                                  <span
+                                      class="catalog__additionally--price"
+                                      >+ 20₽</span
+                                  >
+                                  <input
+                                      class="catalog__additionally--checkbox"
+                                      type="checkbox"
+                                  />
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+                  <div
+                      class="swiper-scrollbar"
+                  ></div>
+              </div>
+          </div>
+          
+
+          <!-- <h3
+          class="catalog__additionally--title"
+      >
+          Дополнительно:
+      </h3>
+      <div
+          class="catalog__additionally--block row"
+      >
+          <span
+              class="catalog__additionally--info"
+              >Побольше лука</span
+          >
+          <div
+              class="catalog__additionally--content"
+          >
+              <span
+                  class="catalog__additionally--price"
+                  >+ 20₽</span
+              >
+              <input
+                  class="catalog__additionally--checkbox"
+                  type="checkbox"
+              />
+          </div>
+      </div> -->
+          <div class="catalog__add">
+              <div class="catalog__btns row">
+                  <span
+                      class="catalog__btn--minus"
+                  ></span>
+                  <b
+                      class="catalog__food--quantity"
+                      >0</b
+                  >
+                  <span
+                      class="catalog__btn--plus"
+                  ></span>
+              </div>
+              <button class="catalog__food--add">
+                  Добавить
+              </button>
+          </div>
+      </div>
+  </div>
+      `;  
+    }
+};
+getData()
+const catalogSwiper = new Swiper(".catalog__swiper", {
+    direction: "vertical",
+    slidesPerView: "auto",
+    freeMode: true,
+    scrollbar: {
+        el: ".swiper-scrollbar",
+    },
+    mousewheel: true,
 });
