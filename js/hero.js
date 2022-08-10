9cbf975021dc8b56f1d9572d524e23af0076719c
saliia <cygnuswinter@gmail.com>
2022-08-10 18:02:55 +0600

    add new files

---
+++ js/hero.js
@@ -0,0 +1,11 @@
+new Swiper('.hero-slider', {
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    slideToClickedSlide: true,
+
+    slidesPerView: 1.1,
+    loop: true,
+})
